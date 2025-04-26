3d490b8ac18e70ca7efce5d0402b61203860a70f
Tutku Figen Güngör <serkangun234@gmail.com>
2025-04-26 22:19:52 +0300

    Unloading resources added and a potential memory leak fixed

--- src/utils/resourceManager.c
+++ src/utils/resourceManager.c
@@ -1,9 +1,15 @@
 #include "raylib.h"
 #include "resourceManager.h"
 
-void LoadResources(resourcemManager *resourceManager)
+void LoadResources(resourceManager *resourceManager)
 {
     resourceManager->ambient = LoadMusicStream("./resources/ocean.mp3");
     Image playerImage = LoadImage("./resources/walk1.png");
     resourceManager->playerTexture = LoadTextureFromImage(playerImage);
+    UnloadImage(playerImage);
+}
+void UnloadResources(resourceManager *resourceManager)
+{
+    UnloadTexture(resourceManager->playerTexture);
+    UnloadMusicStream(resourceManager->ambient);
 }

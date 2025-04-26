05a4cffa4a5f1dca46c62d942980cdf8a7a93b73
Tutku Figen Güngör <serkangun234@gmail.com>
2025-04-27 01:22:04 +0300

    Draw Function partitioned and a build bug has been fixed

--- src/gameStateManager.c
+++ src/gameStateManager.c
@@ -1,6 +1,7 @@
 #include "gameStateManager.h"
-GameStateManager ConstructGameStateManager(GameStateManager gm)
+GameStateManager ConstructGameStateManager()
 {
+    GameStateManager gm;
     gm.currentScreen = SCREEN_TITLE;
     gm.finishGame = false;
     gm.pause = false;
--- src/includes/utils/InputManager.h
+++ src/includes/utils/InputManager.h
@@ -1,4 +1,3 @@
 #include "raylib.h"
-#include "gameStateManager.h"
 void MovePlayer(Rectangle *player, float playerSpeed);
 void PauseGame(GameStateManager *gm);
